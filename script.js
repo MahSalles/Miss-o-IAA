02dad7d405213900d063e608e1164db36aa85cd6
MahSalles <145451172+MahSalles@users.noreply.github.com>
2025-08-14 08:18:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
     {
         enunciado: "Pergunta 1",
@@ -17,5 +18,26 @@ const perguntas = [
             "Alternativa 1",
             "Alternativa 2"
         ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
     }
-]; 
+];
