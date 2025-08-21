6209cedef48432f35cfb80dcef145b2e8c9818d6
gabrieljitkoski <145364710+gabrieljitkoski@users.noreply.github.com>
2025-08-21 07:51:48 -0300

    g

--- script.js
+++ script.js
@@ -8,29 +8,54 @@ const perguntas = [
     {
         enunciado: "Você se deparou com muitas fake news nos meios sociais e acabou encontrando uma IA que analiza as informações e responde se elas são verídicas ou não! O que você acha disso?",
         alternativas: [
-            "Isso é assustador !",
-            "Isso é muito útil !!!"
+            {
+            texto:"Isso é assustador !",
+            afirmacao:"afirmação"
+            },
+            {
+            texto:"Isso é muito útil !!!",
+            afirmacao:"afirmação"
+            }
+            
         ]
     },
     {
         enunciado: "Você começou a utilizar essa IA e achou muito interessante o site, começou a responder as perguntas sobre fake news. Você viu uma notícia que um ator morreu, mas a notícia está em um site estranho, o que você faria?",
         alternativas: [
-            "Usaria a IA que achei",
-            "Repasso para todo mundo por precaução e corro para comentar nas redes sociais.",
+            {
+            texto:"Usaria a IA que achei",
+            afirmacao:"afirmação"
+            },
+            {
+           texto: "Repasso para todo mundo por precaução e corro para comentar nas redes sociais.",
+            afirmacao:"afirmação"
+            }
         ]
     },
     {
         enunciado: "Depois que a IA te respondeu sobre a noticia ser falsa, o que você faz?",
         alternativas: [
-            " Pesquisa em outras fontes se a informação realmente é falsa e a IA é realmente segura para ser utilizada.",
-            "Acredito que a IA é segura e não vou atrás de outras fontes para  confirmar a informação."
+            {
+            texto:"Pesquisa em outras fontes se a informação realmente é falsa e a IA é realmente segura para ser utilizada.",
+            afirmacao:"afirmação"
+            },
+            {
+            texto:"Acredito que a IA é segura e não vou atrás de outras fontes para  confirmar a informação.",
+            afirmacao:"afirmação"
+            }
         ]
     },
     {
         enunciado: "Ao final das perguntas você gostou do site e teve reações positivas, o que você pensa em fazer ?",
         alternativas: [
-            "Repassar o site para seus amigos e nos da uma avaliação positiva",
-            "Gosto do site dou avaliação positiva mas não repasso para ninguém "
+            {
+            texto:"Repassar o site para seus amigos e nos da uma avaliação positiva",
+            afirmacao:"afirmação"
+            },
+            {
+            texto:"Gosto do site dou avaliação positiva mas não repasso para ninguém ",
+            afirmacao:"afirmação"
+            }
         ]
     },
 ];
