fb7fb00ff517962cd0bed1bcae95359fcb5778e2
MahSalles <145451172+MahSalles@users.noreply.github.com>
2025-08-21 07:31:48 -0300

    g

--- script.js
+++ script.js
@@ -6,39 +6,41 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Você se deparou com muitas fake news nos meios sociais e acabou encontrando uma IA que analiza as informações e responde se elas são verídicas ou não! O que você acha disso?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Isso é assustador !",
+            "Isso é muito útil !!!"
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "Você começou a utilizar essa IA e achou muito interessante o site, começou a responder as perguntas sobre fake news. Você viu uma notícia que um ator morreu, mas a notícia está em um site estranho, o que você faria?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Usaria a IA que achei",
+            "Repasso para todo mundo por precaução e corro para comentar nas redes sociais.",
         ]
     },
     {
-        enunciado: "Pergunta 3",
+        enunciado: "Depois que a IA te respondeu sobre a noticia ser falsa, o que você faz?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            " Pesquisa em outras fontes se a informação realmente é falsa e a IA é realmente segura para ser utilizada.",
+            "Acredito que a IA é segura e não vou atrás de outras fontes para  confirmar a informação."
         ]
     },
     {
-        enunciado: "Pergunta 4",
+        enunciado: "Ao final das perguntas você gostou do site e teve reações positivas, o que você pensa em fazer ?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Repassar o site para seus amigos e nos da uma avaliação positiva",
+            "Gosto do site dou avaliação positiva mas não repasso para ninguém "
         ]
     },
-    {
-        enunciado: "Pergunta 5",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
-    }
 ];
 
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
